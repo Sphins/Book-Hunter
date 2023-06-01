517212a7a959228cd81673aa581824165a3ab7bf
sphins <glehenerz@gmail.com>
2023-06-01 15:02:28 +0200

    Intégration et décomposition du template en partials

--- app/views/template/index.php
+++ app/views/template/index.php
@@ -1,1 +1,19 @@
-<h1>coucou</h1>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include_once '../app/views/template/partials/_head.php'; ?>
+</head>
+
+<body class="bg-gray-900 text-white font-sans leading-normal tracking-normal">
+    <!-- Header -->
+    <?php include_once '../app/views/template/partials/_header.php'; ?>
+
+    <!-- Main -->
+    <?php include_once '../app/views/template/partials/_main.php'; ?>
+
+    <!-- Footer -->
+    <?php include_once '../app/views/template/partials/_footer.php'; ?>
+</body>
+
+</html>
